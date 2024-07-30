8b004a2e53a2e482505f38e266a164c4c985bf59
Oleksandra Halieieva <aleksavision@gmail.com>
2024-07-30 17:46:50 +0300

    +changes

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -19,7 +19,7 @@ abstract public class BaseTest {
         Configuration.browser = "chrome";
         Configuration.webdriverLogsEnabled = true;
         Configuration.browserSize = "1920x1080";
-        Configuration.headless = false;
+        Configuration.headless = true;
         Selenide.open(mainUrl);
         homepage = new Homepage();
     }
